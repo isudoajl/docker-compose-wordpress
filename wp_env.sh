172b5db30f6f56f772166c4384cf4e3553362671
isudovm <isudovm@wpdev.lan>
2022-12-08 00:12:04 +0000

    Generate self signed certified

--- wp_env.sh
+++ wp_env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo '*** Generating Certs ***'
+openssl req -x509 -newkey rsa:2048 -days 365 -keyout wordpress-docker.test-key.pem -nodes -out wordpress-docker.test.pem -subj "/C=PT/ST=SINTRA/L=LISBON/O=PIEDAD/OU=IT DEPT/CN=camaster/emailAddress=xxx@gmail.com" 
+
+mv *.pem nginx/certs
+
 echo '*** Downloading wordpress.org source code ***'
 wget http://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
